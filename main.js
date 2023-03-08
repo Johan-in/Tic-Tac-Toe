9c243a47c74e723e0cd8243ab06ca77789825528
Johan <senatovi21@gmail.com>
2023-03-08 14:09:54 +0300

    initialize game  variables with function

--- main.js
+++ main.js
@@ -24,5 +24,24 @@ form.addEventListener("submit", (event) => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
     document.querySelector(".modal-wrapper").setAttribute("hidden", true);
-    console.log(data);
+    initializeGame(data)
 });
+
+const initializeVariables = (data) => {
+    data.choice =  +data.choice;
+    data.board = [0,1,2,3,4,5,6,7,8]
+    data.player1 = "X";
+    data.player2 = "O";
+    data.round = 0;
+    data.currentPlayer = "x";
+    data.gameOver = false;
+}
+
+const initializeGame = (data) => {
+    //initialize game  variables
+    initializeVariables(data);
+
+    console.log(data);
+    //add  event listeners to the gameboard
+    
+};
